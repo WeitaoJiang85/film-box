9393e062cb7bdc6d569fc8454b5f99e42e07633f
WEITAO JIANG <weitaoj@outlook.com>
2022-09-23 00:26:06 +1000

    change code  in FilmLibrary.js

--- src/component/FlimLibrary/FilmLibrary.js
+++ src/component/FlimLibrary/FilmLibrary.js
@@ -1,27 +1,27 @@
-import { useState, useEffect } from 'react'
-import { FilmDetail, FilmDetailEmpty } from '../FilmDetail/FilmDetail'
-import FilmRow from '../FlimRow/FlimRow'
-import Button from '@mui/material/Button'
-import TextField from '@mui/material/TextField'
-import './FilmLibrary.css'
-import Stack from '@mui/material/Stack'
-const API_KEY = process.env.REACT_APP_API_KEY
+import { useState, useEffect } from "react";
+import { FilmDetail, FilmDetailEmpty } from "../FilmDetail/FilmDetail";
+import FilmRow from "../FlimRow/FlimRow";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import "./FilmLibrary.css";
+import Stack from "@mui/material/Stack";
+const API_KEY = process.env.REACT_APP_API_KEY;
 function FilmLibrary() {
-  const [page, setPage] = useState(1)
-  const [input, setInput] = useState(new Date().getFullYear())
-  const [year, setYear] = useState(new Date().getFullYear())
-  const [selectedFilm, setSelectedFilm] = useState(null)
-  const [faveList, setFaveList] = useState([])
-  const [displayAll, setDisplayAll] = useState(true)
-  const [result, setResult] = useState([])
-  const [totalPages, setTotalPages] = useState(1)
+  const [page, setPage] = useState(1);
+  const [input, setInput] = useState(new Date().getFullYear());
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [selectedFilm, setSelectedFilm] = useState(null);
+  const [faveList, setFaveList] = useState([]);
+  const [displayAll, setDisplayAll] = useState(true);
+  const [result, setResult] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     const listURL = `
-  https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=${page}&primary_release_year=${year}`
+  https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=${page}&primary_release_year=${year}`;
     fetch(listURL)
       .then((response) => {
-        return response.json()
+        return response.json();
       })
       .then((jsonData) => {
         if (jsonData.results.length > 0) {
@@ -33,71 +33,71 @@ function FilmLibrary() {
             release_date: film.release_date,
             overview: film.overview,
             fave: false,
-          }))
+          }));
 
-          setResult((r) => [...result, ...searchResult])
+          setResult((r) => [...result, ...searchResult]);
         }
-        setTotalPages(jsonData.total_pages)
+        setTotalPages(jsonData.total_pages);
       })
       .catch((error) => {
-        console.log(error)
-      })
-  }, [year, page])
+        console.log(error);
+      });
+  }, [year, page]);
 
   const handleSeleced = (id) => {
-    setSelectedFilm(result.find((film) => film.id === id))
-  }
+    setSelectedFilm(result.find((film) => film.id === id));
+  };
 
   const loadMore = () => {
     if (page < totalPages) {
-      setPage(page + 1)
+      setPage(page + 1);
     } else {
-      return <p>This is the last page</p>
+      return <p>This is the last page</p>;
     }
-  }
+  };
   const handleYear = () => {
-    if (input !== '') setYear(input)
-    setPage(1)
-    setResult([])
-    setInput('')
-  }
+    if (input !== "") setYear(input);
+    setPage(1);
+    setResult([]);
+    setInput("");
+  };
 
   const showAll = () => {
-    setDisplayAll(true)
-  }
+    setDisplayAll(true);
+  };
   const showFave = () => {
-    setDisplayAll(false)
-  }
+    setDisplayAll(false);
+  };
 
   const addFave = (e, id) => {
-    e.stopPropagation()
-    const faveFilm = result.filter((film) => film.id === id)
+    e.stopPropagation();
+    const faveFilm = result.filter((film) => film.id === id);
     if (faveFilm[0].fave === false) {
-      faveFilm[0].fave = true
+      faveFilm[0].fave = true;
     }
-    setFaveList([...faveList, { ...faveFilm[0] }])
-    console.log(`Favelist`, faveList)
-  }
+    setFaveList([...faveList, { ...faveFilm[0] }]);
+    console.log(`Favelist`, faveList);
+  };
 
   const delFave = (e, id) => {
-    e.stopPropagation()
-    const unFaveFilm = faveList.filter((film) => film.id === id)
+    e.stopPropagation();
+    const unFaveFilm = faveList.filter((film) => film.id === id);
     if (unFaveFilm[0].fave === true) {
-      unFaveFilm[0].fave = false
+      unFaveFilm[0].fave = false;
     }
-    const resFave = faveList.filter((film) => film.id !== id)
-    setFaveList(resFave)
-    console.log(resFave)
+    const resFave = faveList.filter((film) => film.id !== id);
+    setFaveList(resFave);
+    console.log(resFave);
     const newFilms = result.map((film) =>
       film.id === id ? { ...film, fave: false } : film
-    )
-    setResult(newFilms)
-  }
+    );
+    setResult(newFilms);
+  };
 
   return (
     <div className="FilmLibrary">
       <div className="film-list">
-        <h1 className="section-title">FILMS</h1>
+        <h1 className="section-title">Films List</h1>
 
         {displayAll ? (
           <Stack
@@ -115,8 +115,8 @@ function FilmLibrary() {
               value={input}
               onChange={(e) => setInput(e.target.value)}
               sx={{
-                color: 'secondary',
-                padding: '1px',
+                color: "secondary",
+                padding: "1px",
               }}
             />
             <Button
@@ -125,8 +125,8 @@ function FilmLibrary() {
               size="medium"
               onClick={handleYear}
               sx={{
-                fontWeight: 'bold',
-                fontSize: '1em',
+                fontWeight: "bold",
+                fontSize: "1em",
               }}
             >
               SEARCH
@@ -137,14 +137,14 @@ function FilmLibrary() {
         <div className="film-list-filters">
           <button
             onClick={showAll}
-            className={`film-list-filter ${displayAll ? 'is-active' : null}`}
+            className={`film-list-filter ${displayAll ? "is-active" : null}`}
           >
             ALL
             <span className="section-count">{result.length}</span>
           </button>
           <button
             onClick={showFave}
-            className={`film-list-filter ${displayAll ? null : 'is-active'}`}
+            className={`film-list-filter ${displayAll ? null : "is-active"}`}
           >
             FAVES
             <span className="section-count">{faveList.length}</span>
@@ -184,8 +184,8 @@ function FilmLibrary() {
             size="small"
             onClick={loadMore}
             sx={{
-              fontWeight: 'bold',
-              fontSize: '1em',
+              fontWeight: "bold",
+              fontSize: "1em",
             }}
           >
             Load More
@@ -199,7 +199,7 @@ function FilmLibrary() {
         {selectedFilm ? <FilmDetail /> : <FilmDetailEmpty />}
       </div>
     </div>
-  )
+  );
 }
 
-export default FilmLibrary
+export default FilmLibrary;
